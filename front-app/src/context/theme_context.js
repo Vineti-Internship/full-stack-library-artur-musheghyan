a18d91d7fc1f1db97b1f2a166d4ed203d20260cc
artur <artarmmush@gmail.com>
2018-12-01 15:15:48 +0400

    Add editColor to theme, change editBook button color.

--- front-app/src/context/theme_context.js
+++ front-app/src/context/theme_context.js
@@ -6,14 +6,17 @@ export const themes = {
     colorB: '#fbfffc',
     buttonColor: '#97b498',
     textColor: '#000000',
-    warningColor: '#d32f2f'
+    warningColor: '#d32f2f',
+    editColor: '#ffee58'
   },
   dark: {
     colorA: '#263238',
     colorB: '#4f5b62',
     buttonColor: '#000a12',
     textColor: '#ffffff',
-    warningColor: '#c62828'
+    warningColor: '#c62828',
+    editColor: '#80e27e'
+
   }
 };
 
